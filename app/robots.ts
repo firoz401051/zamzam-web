491965707162b00d8ab1aec5de975ea05a810755
firoz <firoz202530@gmail.com>
2026-06-13 16:15:47 +0530

    Fix robots sitemap URL to www

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://zamzamfashionstore.in/sitemap.xml",
+    sitemap: "https://www.zamzamfashionstore.in/sitemap.xml",
   };
 }
