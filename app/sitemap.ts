e406237d53f542d4478c78300fc0ee8073aba118
firoz <firoz202530@gmail.com>
2026-06-13 12:20:30 +0530

    Use canonical www domain in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://zamzamfashionstore.in";
+  const baseUrl = "https://www.zamzamfashionstore.in";
 
   const routes = [
     "",
