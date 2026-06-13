ab46b2cccfcc4d7c67c523c5e2bc763f6610d98d
firoz <firoz202530@gmail.com>
2026-06-13 12:37:01 +0530

    Use canonical

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,8 +15,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/privacy",
     "/returns",
     "/terms",
-    "/blog",
-    "/brands",
     "/category",
     "/products",
     "/deal",
